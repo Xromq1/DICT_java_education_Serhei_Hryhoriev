0ea252b8e5e97a26e49d042e0cff15cdb4881cdc
Serhei <s.s.hryhoriev@student.khai.edu>
2023-11-09 19:04:17 +0200

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is ChatBot Made by Serhei Hryhoriev.");
+        System.out.println("Hello! My name is ChatBot Made by Serheu Hryhoriev.");
         System.out.println("I was created in 2023.");
         System.out.println("Please, remind me your name.");
 
@@ -20,5 +20,12 @@ public class ChatBot {
         int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
         System.out.println("Your age is " + yourAge + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want!");
+
+        int userInp = scanner.nextInt();
+
+        for (int i = 1; i <= userInp; i++) {
+            System.out.println(i + "!");
+        }
     }
 }
