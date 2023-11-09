d6581a5866173448b18899363ed188d2f44bd990
Serhei <s.s.hryhoriev@student.khai.edu>
2023-11-09 18:26:00 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,7 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is ChatBot Made by Serheu Hryhoriev.");
+        System.out.println("I was created in 2023.");
+    }
+}
+
