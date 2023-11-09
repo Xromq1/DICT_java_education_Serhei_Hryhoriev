2db2ab41c1c55b16ebf2e2ffc9c4a100a8b10b45
Serhei <s.s.hryhoriev@student.khai.edu>
2023-11-09 19:00:50 +0200

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is ChatBot Made by Serheu Hryhoriev.");
+        System.out.println("Hello! My name is ChatBot Made by Serhei Hryhoriev.");
         System.out.println("I was created in 2023.");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + yourName + "!");
     }
 }
-
